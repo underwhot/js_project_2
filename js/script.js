d4a0b8b062f4a7de9d6a140abde8900ee4a77368
Artem Petrovskiy <artpetrovskyi@gmail.com>
2023-07-17 21:12:00 +0300

    add dynamic sort movie list

--- js/script.js
+++ js/script.js
@@ -12,15 +12,45 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
+"use strict";
 
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+  movies: [
+    "Логан",
+    "Лига справедливости",
+    "Ла-ла лэнд",
+    "Одержимость",
+    "Скотт Пилигрим против...",
+  ],
 };
 
+const promoAdvEl = document.querySelector(".promo__adv"),
+      advBannersEl = promoAdvEl.querySelectorAll("img"),
+      genreEl = document.querySelector(".promo__genre"),
+      promoBgEl = document.querySelector(".promo__bg"),
+      promoListEl = document.querySelector(".promo__interactive-list"),
+      promoListItemEl = promoListEl.querySelectorAll(".promo__interactive-item");
+
+advBannersEl.forEach(function (banner) {
+  banner.remove();
+});
+
+genreEl.textContent = "Драма";
+
+promoBgEl.style.cssText = "background: url(./img/bg.jpg) center center/cover no-repeat;";
+
+promoListEl.innerHTML = '';
+movieDB['movies'].sort();
+
+movieDB['movies'].forEach(function(item, index) {
+  promoListEl.innerHTML += `
+      <li class="promo__interactive-item">${index+1}. ${item}
+        <div class="delete"></div>
+      </li>
+    `
+  // promoListEl.insertAdjacentHTML('beforeend', `
+  //   <li class="promo__interactive-item">${index+1}. ${item}
+  //     <div class="delete"></div>
+  //   </li>
+  //   `) 
+});
